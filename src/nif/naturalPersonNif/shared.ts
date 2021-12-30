a99d573aa234773c26ff397e2ff8c9ff5a233c8d
josegoval <josegovalmusic@gmail.com>
2021-12-30 22:31:51 +0100

    docs: developing README.md

--- src/nif/naturalPersonNif/shared.ts
+++ src/nif/naturalPersonNif/shared.ts
@@ -3,6 +3,7 @@ export const DNI_CONTROL_LETTERS = "TRWAGMYFPDXBNJZSQVHLCKE";
 /**
  * Checks if the dni control code (letter) provided is valid.
  *
+ * It does include checks for DNI K, L and M.
  * @WARNING It does not check the `DNI_REGEX`.
  * @param dni
  * @returns true for valid input and false for invalid input.
